2ac331937e54861767e7a9604397cfd2d0507636
Jason Baldus <jason.baldus@gmail.com>
2022-12-05 10:04:14 -0500

    My weird take on this

--- bestfriend.py
+++ bestfriend.py
@@ -1,3 +1,8 @@
 first_name = input("What is your name? ")
 print(f"Hello {first_name}!")
 
+quest = input("What is your quest? ")
+print(f"So, you are trying to {quest}?")
+
+airspeed = input("What is the airspeed velocity of a swallow?")
+print("Incorrect! You shall not pass!")
